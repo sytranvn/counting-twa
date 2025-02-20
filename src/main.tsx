e0975fa357a55a526506522aebcf649e37e7d59e
Sy Tran Dung <sytrancsvn@gmail.com>
2025-02-20 14:22:15 +0700

    display count and increment

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
+const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
+    </TonConnectUIProvider>
   </StrictMode>,
 )
