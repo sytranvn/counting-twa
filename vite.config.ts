4c6093b1f489ff8b760b6ae2be88e3d1996a0867
Sy Tran Dung <sytrancsvn@gmail.com>
2025-02-28 11:34:16 +0700

    fix manifest env

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,49 @@
-import { defineConfig } from 'vite'
+import { defineConfig, createLogger, UserConfig, ViteDevServer } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import path from 'path'
 import fs from 'fs/promises'
+import { loadEnv } from 'vite'
 
-function generateManifest() {
+function generateManifest(config: UserConfig) {
+  const env = loadEnv(config.mode!, process.cwd(), 'VITE_')
+  const logger = createLogger()
+  logger.info(config.mode!)
+  const data = {
+    url: env.VITE_APP_URL,
+    name: env.VITE_APP_TITLE,
+    iconUrl: `${env.VITE_APP_URL}/logo.png`
+  }
   return {
     name: 'generate-manifest',
     async writeBundle(options: any) {
-      const data = {
-        url: process.env.VITE_APP_URL,
-        name: process.env.VITE_APP_TITLE,
-        iconUrl: `${process.env.VITE_APP_URL}/logo.png`
-      }
+      await fs.writeFile('log.log', config.mode!)
       const outPath = path.resolve(options.dir, 'tonconnect-manifest.json')
       try {
         await fs.writeFile(outPath, JSON.stringify(data, null, 2), 'utf8');
-        console.log('\ntonconnect-manifest.json')
       } catch (err) {
-        console.error('Error generating tonconnect-manifest.json', err);
+        logger.error('Error generating tonconnect-manifest.json ' + err, { timestamp: true });
       }
+    },
+    configureServer(server: ViteDevServer) {
+      server.middlewares.use(async (req, res, next) => {
+        if (req.url === '/tonconnect-manifest.json') {
+
+          res.writeHead(200, { 'content-type': 'application/json' });
+          res.end(JSON.stringify(data));
+        } else {
+          next();
+        }
+      })
     }
   }
 }
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [generateManifest(), react(), nodePolyfills(), ],
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), nodePolyfills(), generateManifest({ mode })],
   base: '/',
   build: {
     chunkSizeWarningLimit: 5000,
   },
-})
+}))
